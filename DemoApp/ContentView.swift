6224e59b66dcb5c0fcd51dd2e9899e297a4804c6
kaushal <k.kaushal1789@gmail.com>
2019-09-22 10:48:35 +0530

    Initial commit

---
+++ DemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoApp
+//
+//  Created by Kaushlendra Yadav on 22/09/19.
+//  Copyright © 2019 Kaushlendra Yadav. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
